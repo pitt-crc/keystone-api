4e614297fd27f53799de5e120704c4983d51dc0d
Daniel Perrefort <djperrefort@gmail.com>
2023-11-12 15:29:05 -0500

    Cleanup pass for jwt app

--- crc_service_portal/apps/jwt/urls.py
+++ crc_service_portal/apps/jwt/urls.py
@@ -6,6 +6,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 app_name = 'jwt'
 
 urlpatterns = [
-    path('', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('new/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
