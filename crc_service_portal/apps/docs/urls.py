1248b9c6dbeb129a8e2f4d05f8e1950ffa06d376
Daniel Perrefort <djperrefort@gmail.com>
2023-11-12 15:32:35 -0500

    Cleanup pass for docs app

--- crc_service_portal/apps/docs/urls.py
+++ crc_service_portal/apps/docs/urls.py
@@ -1,21 +1,12 @@
 """URL routing for the parent application"""
 
 from django.urls import path
-from django.views.generic import TemplateView
-from rest_framework.schemas import get_schema_view
+
+from .views import *
 
 app_name = 'docs'
 
 urlpatterns = [
-    path('openapi', get_schema_view(
-        title="Your Project",
-        description="API for all things …",
-        version="1.0.0"
-    ), name='openapi-schema'),
-
-    # This entry assumes the application is installed under the namespace `docs`
-    path('', TemplateView.as_view(
-        template_name='api_docs/redoc.html',
-        extra_context={'schema_url': 'docs:openapi-schema'}
-    ), name='redoc'),
+    path('openapi', SchemaView, name='openapi-schema'),
+    path('', RedocView, name='redoc'),
 ]
---
+++ crc_service_portal/apps/docs/views.py
@@ -0,0 +1,23 @@
+"""Application logic for rendering HTML templates and handling HTTP requests.
+
+View objects handle the processing of incoming HTTP requests and return the
+appropriately rendered HTML template or other HTTP response.
+"""
+
+from django.conf import settings
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
+__all__ = ['SchemaView', 'RedocView']
+
+SchemaView = get_schema_view(
+    title="CRC Self Service API",
+    description="An API for administrating user resources on HPC systems.",
+    version=settings.VERSION
+)
+
+# The `schema_url` assumes the application is installed under the namespace `docs`
+RedocView = TemplateView.as_view(
+    template_name='api_docs/redoc.html',
+    extra_context={'schema_url': 'docs:openapi-schema'}
+)
