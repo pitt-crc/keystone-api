e2bada3e2262231d73d07758afcb2f3f3641a79d
Daniel Perrefort <djperrefort@gmail.com>
2023-11-12 13:37:19 -0500

    Adds application for API docs

--- crc_service_portal/main/urls.py
+++ crc_service_portal/main/urls.py
@@ -6,10 +6,12 @@ from django.views.generic import TemplateView
 
 from apps.health.views import HealthCheckView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('health/', HealthCheckView.as_view(), name='health_check_custom'),
+    path('docs/', include('apps.docs.urls', namespace='docs')),
     path('alloc/', include('apps.allocations.urls', namespace='alloc')),
     path('auth/', include('apps.jwt.urls', namespace='jwt')),
     path('prod/', include('apps.research_products.urls', namespace='research_products')),
