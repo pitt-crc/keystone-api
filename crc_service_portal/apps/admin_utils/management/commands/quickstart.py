2c88e34edc1ddc1a36f984b6703e4a8fd295281d
Daniel Perrefort <djperrefort@gmail.com>
2023-11-13 15:17:57 -0700

    Fixes bug in no-input options

--- crc_service_portal/apps/admin_utils/management/commands/quickstart.py
+++ crc_service_portal/apps/admin_utils/management/commands/quickstart.py
@@ -20,7 +20,6 @@ to handle database migrations, static file collection, and web server deployment
 import subprocess
 from argparse import ArgumentParser
 
-from django.core.asgi import get_asgi_application
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
@@ -40,12 +39,11 @@ class Command(BaseCommand):
         parser.add_argument('--migrate', action='store_true', help='Run database migrations.')
         parser.add_argument('--no-input', action='store_true', help='Do not prompt for user input of any kind.')
 
-        webserver = parser.add_argument_group('Web Server')
-        server_type = webserver.add_mutually_exclusive_group()
+        server_type = parser.add_mutually_exclusive_group()
         server_type.add_argument('--gunicorn', action='store_true', help='Run a web server using Gunicorn.')
         server_type.add_argument('--uvicorn', action='store_true', help='Run a web server using Uvicorn.')
-        parser.add_argument('--host', default='0.0.0.0')
-        parser.add_argument('--port', type=int, default=8000)
+        parser.add_argument('--host', default='0.0.0.0', help='Bind socket to this host.')
+        parser.add_argument('--port', type=int, default=8000, help='Bind socket to this port.')
 
     def handle(self, *args, **options):
         """Handle the command execution.
@@ -58,11 +56,11 @@ class Command(BaseCommand):
 
         if options['migrate']:
             self.stdout.write(self.style.SUCCESS('Running database migrations...'))
-            call_command('migrate', no_input=options['no_input'])
+            call_command('migrate', no_input=not options['no_input'])
 
         if options['static']:
             self.stdout.write(self.style.SUCCESS('Collecting static files...'))
-            call_command('collectstatic', no_input=options['no_input'])
+            call_command('collectstatic', no_input=not options['no_input'])
 
         if options['gunicorn']:
             self.stdout.write(self.style.SUCCESS('Starting Gunicorn server...'))
@@ -80,8 +78,8 @@ class Command(BaseCommand):
           port: The port to bind to
         """
 
-        gunicorn_command = ['gunicorn', '--bind', f'{host}:{port}', 'crc_service_portal.main.wsgi:application', ]
-        subprocess.run(gunicorn_command, check=True)
+        command = ['gunicorn', '--bind', f'{host}:{port}', 'crc_service_portal.main.wsgi:application', ]
+        subprocess.run(command, check=True)
 
     def run_uvicorn(self, host: str = '0.0.0.0', port: int = 8000):
         """Start a Uvicorn server.
@@ -91,5 +89,5 @@ class Command(BaseCommand):
           port: The port to bind to
         """
 
-        import uvicorn
-        uvicorn.run(get_asgi_application(), host=host, port=port)
+        command = ['uvicorn', 'crc_service_portal.main.wsgi:application', '--host', host, '--port', str(port)]
+        subprocess.run(command, check=True)
