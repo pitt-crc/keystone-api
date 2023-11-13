ba12f270b3fc7bcca3f1f527971de28223d3667d
Daniel Perrefort <djperrefort@gmail.com>
2023-11-13 11:25:55 -0700

    Adds example tasks/utils for slurm

--- crc_service_portal/apps/slurm/tasks.py
+++ crc_service_portal/apps/slurm/tasks.py
@@ -3,8 +3,6 @@
 from celery import shared_task
 
 
-@shared_task(name='Example Task')
-def example_task():
-    """This is an example"""
-
-    print("The example task just ran.")
+@shared_task(name='Update Account Status')
+def update_account_status():
+    """Lock/unlock user accounts based on the status of their SU allocation"""
