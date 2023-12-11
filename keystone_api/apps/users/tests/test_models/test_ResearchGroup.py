e5268f450989687e6f70283845a09e29d29ec8ce
Daniel Perrefort <djperrefort@gmail.com>
2023-12-11 13:35:00 -0500

    Test simplification

--- keystone_api/apps/users/tests/test_models/test_ResearchGroup.py
+++ keystone_api/apps/users/tests/test_models/test_ResearchGroup.py
@@ -1,6 +1,5 @@
 """Tests for the `ResearchGroup` model"""
 
-from django.db import IntegrityError
 from django.test import TestCase
 
 from apps.users.models import ResearchGroup, User
@@ -18,35 +17,8 @@ class GetAllMembers(TestCase):
         self.unprivileged1 = User.objects.create_user(username='unprivileged1')
         self.unprivileged2 = User.objects.create_user(username='unprivileged2')
 
-    def test_pi_only(self) -> None:
-        """Test returned group members for a group with a PI only"""
-
-        group = ResearchGroup.objects.create(pi=self.pi)
-        expected_members = (self.pi,)
-        self.assertEqual(expected_members, group.get_all_members())
-
-    def test_pi_with_admins(self) -> None:
-        """Test returned group members for a group with a PI and admins"""
-
-        group = ResearchGroup.objects.create(pi=self.pi)
-        group.admins.add(self.admin1)
-        group.admins.add(self.admin2)
-
-        expected_members = (self.pi, self.admin1, self.admin2)
-        self.assertEqual(expected_members, group.get_all_members())
-
-    def test_pi_with_unprivileged(self) -> None:
-        """Test returned group members for a group with a PI and unprivileged users"""
-
-        group = ResearchGroup.objects.create(pi=self.pi)
-        group.unprivileged.add(self.unprivileged1)
-        group.unprivileged.add(self.unprivileged2)
-
-        expected_members = (self.pi, self.unprivileged1, self.unprivileged2)
-        self.assertEqual(expected_members, group.get_all_members())
-
-    def test_pi_with_admin_and_unprivileged(self) -> None:
-        """Test returned group members for a group with a PI, admins, and unprivileged users"""
+    def test_all_accounts_returned(self) -> None:
+        """Test all group members are included in the returned list"""
 
         group = ResearchGroup.objects.create(pi=self.pi)
         group.admins.add(self.admin1)
