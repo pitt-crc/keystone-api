1136198161238472cd7bb57d0fca381ae64cb952
Daniel Perrefort <djperrefort@gmail.com>
2023-12-13 11:33:01 -0500

    Adds warning when calling mansage.py directly

--- keystone_api/manage.py
+++ keystone_api/manage.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Command-line utility for executing administrative tasks."""
 
 import sys
+from warnings import warn
 
 from django.core.management import execute_from_command_line
 
 
-def main():
+def main() -> None:
     """Parse the commandline and run administrative tasks."""
 
     execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
+    warn("You are calling `manage.py' directly. Use the bundled `keystone-api` command instead.", RuntimeWarning)
     main()
