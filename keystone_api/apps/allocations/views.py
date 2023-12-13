786d22a3f333b5791dde084f6939ebf6d944acd6
Daniel Perrefort <djperrefort@gmail.com>
2023-12-13 12:20:24 -0500

    Add handling of admin users to endpoint filtering

--- keystone_api/apps/allocations/views.py
+++ keystone_api/apps/allocations/views.py
@@ -28,26 +28,54 @@ class AllocationViewSet(viewsets.ModelViewSet):
     filterset_fields = '__all__'
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
+    def get_queryset(self) -> list[Allocation]:
+        """Return a list of allocations for the currently authenticated user
+
+        Admin users are returned a list of all allocations across all users.
+        """
+
+        if self.request.user.is_staff() or self.request.user.is_superuser():
+            return Allocation.objects.all()
+
         research_groups = ResearchGroup.objects.groups_for_user(self.request.user)
         return Allocation.objects.filter(proposal__group__in=research_groups).all()
 
 
 class ProposalViewSet(viewsets.ModelViewSet):
-    """Manage project proposals used to request additional service unit allocations."""
+    """Manage project proposals submitted by users to request additional service unit allocations."""
 
     serializer_class = ProposalSerializer
     filterset_fields = '__all__'
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
+    def get_queryset(self) -> list[Proposal]:
+        """Return a list of proposals for the currently authenticated user
+
+        Admin users are returned a list of all proposals across all users.
+        """
+
+        if self.request.user.is_staff() or self.request.user.is_superuser():
+            return Proposal.objects.all()
+
         research_groups = ResearchGroup.objects.groups_for_user(self.request.user)
-        return Allocation.objects.filter(group__in=research_groups).all()
+        return Proposal.objects.filter(group__in=research_groups).all()
 
 
 class ProposalReviewViewSet(viewsets.ModelViewSet):
-    """Manage project proposal reviews."""
+    """Manage project proposal reviews submitted by administrators."""
 
     queryset = ProposalReview.objects.all()
     serializer_class = ProposalReviewSerializer
     filterset_fields = '__all__'
+
+    def get_queryset(self) -> list[Allocation]:
+        """Return a list of proposal reviews for the currently authenticated user
+
+        Admin users are returned a list of all records across all users.
+        """
+
+        if self.request.user.is_staff() or self.request.user.is_superuser():
+            return ProposalReview.objects.all()
+
+        research_groups = ResearchGroup.objects.groups_for_user(self.request.user)
+        return ProposalReview.objects.filter(proposal__group__in=research_groups).all()
