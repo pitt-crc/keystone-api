398057222bc0475fce9234604ebf8c5769babbf4
Daniel Perrefort <djperrefort@gmail.com>
2024-01-03 13:05:57 -0500

    Adds permission class for allocations

--- keystone_api/apps/allocations/permissions.py
+++ keystone_api/apps/allocations/permissions.py
@@ -8,7 +8,9 @@ predefined access rules.
 
 from rest_framework import permissions
 
-__all__ = ['StaffWriteAuthenticatedRead']
+from .models import RGAffiliatedModel
+
+__all__ = ['StaffWriteAuthenticatedRead', 'StaffWriteGroupRead']
 
 
 class StaffWriteAuthenticatedRead(permissions.BasePermission):
@@ -24,3 +26,27 @@ class StaffWriteAuthenticatedRead(permissions.BasePermission):
             return request.user.is_authenticated
 
         return request.user.is_staff or request.method == 'TRACE'
+
+
+class StaffWriteGroupRead(permissions.BasePermission):
+    """
+    Read access is granted to all users belonging to the same research group
+    as the requested object. Staff users retain all read/write permissions.
+    """
+
+    def has_permission(self, request, view) -> bool:
+        """Return whether the request has permissions to access the requested resource"""
+
+        if request.method in permissions.SAFE_METHODS:
+            return request.user.is_authenticated
+
+        return request.user.is_staff or request.method == 'TRACE'
+
+    def has_object_permission(self, request, view, obj: RGAffiliatedModel) -> bool:
+        """Return whether the incoming HTTP request has permission to access a database record"""
+
+        if request.user.is_staff or request.method == 'TRACE':
+            return True
+
+        user_is_in_group = request.user in obj.get_research_group().get_all_members()
+        return request.method in permissions.SAFE_METHODS and user_is_in_group
