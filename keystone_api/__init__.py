389c889bea1ccc0cb647d0fb6cc17cda943817c6
Daniel Perrefort <djperrefort@gmail.com>
2024-02-02 14:18:02 -0500

    Fixes django init error

--- keystone_api/__init__.py
+++ keystone_api/__init__.py
@@ -5,5 +5,6 @@ from .apps.scheduler.celery import celery_app
 # Default to using the packaged application settings file
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'keystone_api.main.settings')
 
-# The celery application needs to be imported when Django starts
+# Including the celery application object in __all__ is a celery requirement
+# and ensures shared tasks use th appropriate celery instance.
 __all__ = ('celery_app', 'apps')
