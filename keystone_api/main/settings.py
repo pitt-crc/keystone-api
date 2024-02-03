567f07887b9d9f0c3b14da2a044406e307ca058b
Daniel Perrefort <djperrefort@gmail.com>
2024-02-03 18:05:06 -0500

    Fix typo in URL construction

--- keystone_api/main/settings.py
+++ keystone_api/main/settings.py
@@ -166,7 +166,7 @@ _redis_host = env.url('REDIS_HOST', 'redis://127.0.0.1').geturl()
 _redis_port = env.int('REDIS_PORT', 6379)
 _redis_db = env.int('REDIS_DB', 0)
 
-REDIS_URL = f'{_redis_host}:{_redis_port}/{_redis_db}'
+REDIS_URL = f'{_redis_host}:{_redis_port}'
 CELERY_BROKER_URL = REDIS_URL.rstrip('/') + f'/{_redis_db}'
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 CELERY_CACHE_BACKEND = 'django-cache'
