96f6c7d988966814de86bd04a92764e03be6a54f
comeani <nlc60@pitt.edu>
2024-02-13 14:41:31 -0500

    Start adjusting update status to use sshare

--- keystone_api/apps/allocations/tasks.py
+++ keystone_api/apps/allocations/tasks.py
@@ -10,6 +10,9 @@ from apps.allocations.models import *
 from apps.users.models import *
 
 
+#TODO: Upon creation of a new allocation, update the GrpTresRunMinutes Limit
+
+@shared_task()
 def update_status() -> None:
     """Update the account status on all clusters"""
 
@@ -18,25 +21,26 @@ def update_status() -> None:
         update_status_for_cluster(cluster)
 
 
+@shared_task()
 def update_status_for_cluster(cluster: Cluster) -> None:
     """Update the status of each account on a given cluster"""
 
     # Update status for each individual account
     for account_name in get_account_names():
-        update_status_for_account(cluster.name, account_name)
+        update_status_for_account(cluster, account_name)
 
 
 def update_status_for_account(cluster: Cluster, account_name: str) -> None:
     """Check an accounts resource limits in SLURM against their usage, locking on the cluster if necessary"""
 
     # TODO: Logging?
-
+    # TODO: Need to make sure investment partitions are not locked for relevant slurm accounts
     # Lock account if it does not exist
     try:
         account = ResearchGroup.objects.get(name=account_name)
     except ResearchGroup.DoesNotExist:
         # Lock the account on this cluster
-        set_lock_state(cluster.name, account_name)
+        lock_account_on_cluster(cluster.name, account_name)
         return
 
     # TODO: Should there be a check here for Allocations that expire today? Need to set final usage on those...
@@ -44,20 +48,22 @@ def update_status_for_account(cluster: Cluster, account_name: str) -> None:
 
     # Determine the earliest start date across proposals containing active allocations on the cluster
     end = date.today()
-    active_allocations = Allocation.objects.filter(proposal__group=account,
-                                                   cluster=cluster,
-                                                   proposal__approved=True,
-                                                   proposal__active__lte=date.today(),
-                                                   proposal__expire__gt=date.today()).all()
-    # TODO: double check cluster=cluster works, also make sure we handle when it comes back as None
+    active_allocations_query = Allocation.objects.filter(proposal__group=account,
+                                                         cluster=cluster,
+                                                         proposal__approved=True,
+                                                         proposal__active__lte=date.today(),
+                                                         proposal__expire__gt=date.today()).order_by("proposal__expire")
 
-    # Use the start date of the oldest non-expired proposal
-    start = min(alloc.proposal.active for alloc in active_allocations)
+    # TODO: compare rawusage against limit
+
+    # TODO: double check cluster=cluster works, also make sure we handle when it comes back as None
+    start = active_allocations_query
 
     total_cluster_usage = get_cluster_usage(account_name, cluster.name, start, end)
-
     # Determine the total SUs available on the cluster across the active allocations
-    total_cluster_sus = active_allocations.aggregate(Sum("awarded"))
+    total_cluster_sus = active_allocations_query.aggregate(Sum("awarded"))
+
+
 
     if total_cluster_usage >= total_cluster_sus:
 
@@ -68,10 +74,9 @@ def update_status_for_account(cluster: Cluster, account_name: str) -> None:
         set_lock_state(True, cluster.name, account_name)
 
 
-def set_lock_state(lock_state: bool, cluster: str, account: str) -> None:
-    """Update the locked/unlocked state for the given account, on a given cluster"""
-
-    lock_state_int = 0 if lock_state else -1
+def lock_account_on_cluster(cluster: str, account: str) -> None:
+    """Lock the given account, on a given cluster"""
+    cmd = (f'sshare -nP -A {account} -M {cluster} --format=RawUsage')
 
     # Lock/Unlock CPU resources on the cluster
     cmd = (f'sacctmgr -i modify account where account={account} cluster={cluster} '
@@ -88,12 +93,13 @@ def get_account_names() -> List[str]:
     """Get a list of account names for a given cluster"""
 
     # Gather all user account information
+    # For sacctmgr, "n" corresponds to no header rows, P is parsable
+    # TODO: Should this pull info at the per cluster level?
     cmd = "sacctmgr show -nP account format=Account"
     out = subprocess.check_output(cmd, shell=True)
     accounts = out.decode("utf-8").strip()
-    accounts = accounts.split()
 
-    return accounts
+    return accounts.split()
 
 
 def get_cluster_usage(account_name: str, cluster_name: str, start: date, end: date) -> int:
