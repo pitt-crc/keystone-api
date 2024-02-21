51f87f1535b2629bbd3ba067a9e05acf4c4f21ac
Comeani <nlc60@pitt.edu>
2024-02-21 17:14:51 -0500

    Complete rework of limit update logic over individual Slurm accounts, use Popen for Slurm commands instead of subprocess with shell=True

--- keystone_api/apps/allocations/tasks.py
+++ keystone_api/apps/allocations/tasks.py
@@ -2,7 +2,8 @@
 
 from datetime import date
 import re
-import subprocess
+from shlex import split
+from subprocess import PIPE, Popen
 from typing import List
 
 from celery import shared_task
@@ -29,10 +30,10 @@ def update_limits_for_cluster(cluster: Cluster) -> None:
 
 
 @shared_task()
-def update_limit_for_account(account_name: str, cluster_name: str) -> None:
+def update_limit_for_account(account_name: str, cluster: Cluster) -> None:
     """Update the usage limits for an individual Slurm account"""
 
-    # TODO: Check that the Slurm account has an entry in the keystone database
+    # Check that the Slurm account has an entry in the keystone database
     try:
         account = ResearchGroup.objects.get(name=account_name)
     except ResearchGroup.DoesNotExist:
@@ -43,20 +44,31 @@ def update_limit_for_account(account_name: str, cluster_name: str) -> None:
     # Set initial usage for the Slurm Account. If this is their first allocation on the cluster,
     # set it to their current usage
     if not account.initial_usage:
-        account.initial_usage = get_cluster_usage(account.name, cluster_name)
+        account.initial_usage = get_cluster_usage(account.name, cluster.name)
     initial_usage = account.initial_usage
 
-    # TODO: Close out any allocations have ended?
-    close_out_allocations = Allocation.objects.filter(proposal__group=account,
-                                                      cluster=cluster_name,
-                                                      proposal_approved=True,
-                                                      final=None,
-                                                      proposal__expire__lte=date.today()).order_by("proposal__expire")
+    # Gather the historical usage from expired proposal allocations
+    historical_usage = (Allocation.objects.filter(proposal__group=account,
+                                                  cluster=cluster,
+                                                  proposal__approved=True,
+                                                  proposal__active__lte=date.today(),
+                                                  proposal__expire__lte=date.today())
+                        .exclude(final=None).aggregate(Sum("final")))
+
+    # Get the usage total on the cluster for the account from Slurm
+    total_usage = get_cluster_usage(account_name, cluster.name)
+
+    # Close out any expired allocations that have not already been closed out first
+    # Gather allocations that have expired but do not have a final usage value
+    allocations_to_close = Allocation.objects.filter(proposal__group=account,
+                                                     cluster=cluster,
+                                                     proposal_approved=True,
+                                                     final=None,  # TODO: will this work?
+                                                     proposal__expire__lte=date.today()).order_by("proposal__expire")
 
     # Cover as much of the current usage as possible with expired allocations that have not yet been closed out
-    for allocation in close_out_allocations:
-        # TODO: fill out this calculation
-        current_usage = total_cluster_usage - historical_usage - initial_usage
+    for allocation in allocations_to_close:
+        current_usage = total_usage - historical_usage - initial_usage
 
         # Set the final usage for the expired allocation
         if current_usage < allocation.awarded:
@@ -64,35 +76,26 @@ def update_limit_for_account(account_name: str, cluster_name: str) -> None:
         else:
             allocation.final = allocation.awarded
 
+        # Update the historical usage to reflect the closed out allocation
+        historical_usage += allocation.final
+
     # Gather all allocations belonging to active proposals for the account
     active_allocations_query = Allocation.objects.filter(proposal__group=account,
-                                                         cluster=cluster_name,
+                                                         cluster=cluster,
                                                          proposal_approved=True,
                                                          proposal__active__lte=date.today(),
                                                          final=None,
                                                          proposal__expire__gt=date.today()).order_by("proposal__expire")
 
-    # Gather the historical usage from expired proposal allocations
-    historical_usage = Allocation.objects.filter(proposal__group=account_name,
-                                                 cluster=cluster_name,
-                                                 proposal__approved=True,
-                                                 proposal__active__lte=date.today(),
-                                                 proposal__expire__lte=date.today()).aggregate(Sum("final"))
-
-    # Gather total service units available from active proposal allocations
-    proposal_sus = active_allocations_query.aggregate(Sum("awarded"))
-
-    # Get the usage total on the cluster for the account from Slurm
-    total_usage = get_cluster_usage(account_name, cluster_name)
-
-    # Get the current limit
-    current_limit = get_cluster_limit(account_name, cluster_name)
-
     # Calculate the new limit for the account given the current usage and state of their allocations
-    new_limit = calculate_new_limit(current_limit, proposal_sus, historical_usage, initial_usage, total_usage)
+    new_limit = calculate_new_limit(current_limit=get_cluster_limit(account_name, cluster.name),
+                                    proposal_sus=active_allocations_query.aggregate(Sum("awarded")),
+                                    historical_usage=historical_usage,
+                                    initial_usage=initial_usage,
+                                    total_usage=total_usage)
 
     # Set the new limit to the calculated limit
-    set_cluster_limit(account_name, cluster_name, new_limit)
+    set_cluster_limit(account_name, cluster.name, new_limit)
 
 
 def calculate_new_limit(current_limit: int, proposal_sus: int, historical_usage: int, initial_usage: int, total_usage: int) -> int:
@@ -104,8 +107,12 @@ def calculate_new_limit(current_limit: int, proposal_sus: int, historical_usage:
 def get_accounts_on_cluster(cluster_name: str) -> List[str]:
     """Return a list of account names for a given cluster"""
 
-    cmd = "sacctmgr show -nP account withassoc where parents=root cluster={cluster_name} format=Account"
-    out = subprocess.check_output(cmd,shell=True)
+    cmd = f"sacctmgr show -nP account withassoc where parents=root cluster={cluster_name} format=Account"
+    out, err = Popen(split(cmd), stdout=PIPE, stderr=PIPE).communicate()
+
+    if err:
+        pass
+        # TODO: log and raise an error
 
     return out.decode("utf-8").strip().split()
 
@@ -113,10 +120,17 @@ def get_accounts_on_cluster(cluster_name: str) -> List[str]:
 def set_cluster_limit(account_name: str, cluster_name: str, limit: int) -> None:
     """Update the current usage limit (in minutes) to the provided limit on a given cluster for a given account"""
 
-    # TODO: Does this need to be run as root?
+    # TODO: This needs to be run as root... use slurm user?
+    # [nlc60@login0 ~] : sacctmgr -i modify account where account=test cluster=htc set GrpTresMins=billing=0
+    # sacctmgr: error: Your user doesn't have privilege to perform this action
+    #  Error with request: Access/permission denied
     cmd = (f"sacctmgr modify account where account={account_name} cluster={cluster_name} set "
            f"GrpTresRunMins=billing={limit}")
-    subprocess.run(cmd, shell=True)
+    out, err = Popen(split(cmd), stdout=PIPE, stderr=PIPE).communicate()
+
+    if err:
+        pass
+        # TODO: log and raise an error
 
 
 def get_cluster_limit(account_name: str, cluster_name: str) -> int:
@@ -124,8 +138,13 @@ def get_cluster_limit(account_name: str, cluster_name: str) -> int:
 
     # TODO: reintroduce popen/PIPE format to manage security risk of escalating commands with shell=True
     cmd = f"sacctmgr show -nP association where account={account_name} cluster={cluster_name} format=GrpTRESRunMin"
-    out = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    limit = re.findall(r'billing=(.*)\n', out)[0]
+    out, err = Popen(split(cmd), stdout=PIPE, stderr=PIPE).communicate()
+
+    if err:
+        pass
+        # TODO: log and raise an error
+
+    limit = re.findall(r'billing=(.*)\n', out.decode("utf-8"))[0]
 
     return int(limit) if limit.isnumeric() else 0
 
@@ -134,7 +153,12 @@ def get_cluster_usage(account_name: str, cluster_name: str) -> int:
     """Get the total billable usage in minutes on a given cluster for a given account"""
 
     cmd = f"sshare -nP -A {account_name} -M {cluster_name} --format=GrpTRESRaw"
-    out = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    usage = re.findall(r'billing=(.*),fs', out)[0]
+    out, err = Popen(split(cmd), stdout=PIPE, stderr=PIPE).communicate()
+
+    if err:
+        pass
+        # TODO: log and raise an error
+
+    usage = re.findall(r'billing=(.*),fs', out.decode("utf-8"))[0]
 
     return int(usage) if usage.isnumeric() else 0
