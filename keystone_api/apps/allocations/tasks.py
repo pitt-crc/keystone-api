107e158a57766d30a00e92aa0ff667591448a897
Daniel Perrefort <djperrefort@gmail.com>
2024-02-29 13:29:25 -0500

    Adds tls and binding support

--- keystone_api/apps/allocations/tasks.py
+++ keystone_api/apps/allocations/tasks.py
@@ -1,4 +1,9 @@
-"""Schedule tasks executed in parallel by Celery."""
+"""Scheduled tasks executed in parallel by Celery.
+
+Tasks are scheduled and executed in the background by Celery. They operate
+asynchronously from the rest of the application and log their results in the
+application database.
+"""
 
 from datetime import date
 import logging
--- keystone_api/apps/users/tasks.py
+++ keystone_api/apps/users/tasks.py
@@ -1,3 +1,10 @@
+"""Scheduled tasks executed in parallel by Celery.
+
+Tasks are scheduled and executed in the background by Celery. They operate
+asynchronously from the rest of the application and log their results in the
+application database.
+"""
+
 import ldap
 from celery import shared_task
 from django.conf import Settings
@@ -6,12 +13,23 @@ from django_auth_ldap.backend import LDAPBackend
 
 @shared_task()
 def sync_users_with_ldap() -> None:
+    """Update the user database with the latest data from LDAP
+
+    This function performs no action if the `AUTH_LDAP_SERVER_URI` setting
+    is not configured in the application settings.
+    """
+
     if not Settings.AUTH_LDAP_SERVER_URI:
         return
 
     conn = ldap.initialize(Settings.AUTH_LDAP_SERVER_URI)
-    lsearch = conn.search_s(Settings.AUTH_LDAP_USER_SEARCH, ldap.SCOPE_SUBTREE, '(objectClass=account)')
-    ldap_usernames = set(result[1]['uid'][0] for result in lsearch)
+    if Settings.AUTH_LDAP_BIND_DN:
+        conn.bind(Settings.AUTH_LDAP_BIND_DN, Settings.AUTH_LDAP_BIND_PASSWORD)
 
-    for user in ldap_usernames:
-        LDAPBackend().populate_user(user)
+    if Settings.AUTH_LDAP_START_TLS:
+        conn.start_tls_s()
+
+    search = conn.search_s(Settings.AUTH_LDAP_USER_SEARCH, ldap.SCOPE_SUBTREE, '(objectClass=account)')
+    for result in search:
+        for uid in result[1]['uid']:
+            LDAPBackend().populate_user(uid)
