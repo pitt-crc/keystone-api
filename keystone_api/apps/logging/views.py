dd70bb2263c3e24bb7772ca7866687e72c3c12cc
Daniel Perrefort <djperrefort@gmail.com>
2024-03-11 21:45:58 -0400

    Adds API tests

--- keystone_api/apps/logging/views.py
+++ keystone_api/apps/logging/views.py
@@ -4,7 +4,7 @@ View objects handle the processing of incoming HTTP requests and return the
 appropriately rendered HTML template or other HTTP response.
 """
 
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .models import *
 from .serializers import *
