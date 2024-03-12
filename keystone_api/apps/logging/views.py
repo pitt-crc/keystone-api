13f5d1958e17e5d60aaee65e5191c08e3a7f4e32
Daniel Perrefort <djperrefort@gmail.com>
2024-03-11 20:36:37 -0400

    Adds API endpoints

---
+++ keystone_api/apps/logging/views.py
@@ -0,0 +1,21 @@
+"""Application logic for rendering HTML templates and handling HTTP requests.
+
+View objects handle the processing of incoming HTTP requests and return the
+appropriately rendered HTML template or other HTTP response.
+"""
+
+from rest_framework import viewsets
+
+from .models import *
+from .serializers import *
+
+__all__ = ['LogEntryViewSet']
+
+
+class LogEntryViewSet(viewsets.ReadOnlyModelViewSet):
+    """Returns application log data"""
+
+    queryset = LogEntry.objects.all()
+    serializer_class = LogEntrySerializer
+    filterset_fields = '__all__'
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
