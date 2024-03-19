2f2d4c54039201c234ef7c6d668187112fa8ef5f
Comeani <nlc60@pitt.edu>
2024-03-19 17:19:22 -0400

    remove unnecessary override

--- keystone_api/apps/users/views.py
+++ keystone_api/apps/users/views.py
@@ -4,8 +4,7 @@ View objects handle the processing of incoming HTTP requests and return the
 appropriately rendered HTML template or other HTTP response.
 """
 
-from rest_framework import permissions, status, viewsets
-from rest_framework.response import Response
+from rest_framework import permissions, viewsets
 
 from .models import *
 from .serializers import *
@@ -22,18 +21,6 @@ class ResearchGroupViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAdminUser]
     serializer_class = ResearchGroupSerializer
     filterset_fields = '__all__'
-
-    def create(self, request, *args, **kwargs) -> Response:
-        """Create a new `ResearchGroup` object"""
-
-        data = request.data.copy()
-
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class UserViewSet(viewsets.ModelViewSet):
