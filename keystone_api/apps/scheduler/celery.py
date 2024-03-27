243a014e110a660680c503273921cca7091abc9b
Daniel Perrefort <djperrefort@gmail.com>
2024-03-26 21:59:37 -0400

    Fixes bug where default the defult task schdule is ignored. (#226)
    * Fixes task name in celery schedule
    
    * Moves beat schedule into celery.py
    
    * Adds application descriptions

--- keystone_api/apps/scheduler/celery.py
+++ keystone_api/apps/scheduler/celery.py
@@ -7,7 +7,21 @@ with the Celery application instance for all applications defined in the
 """
 
 from celery import Celery
+from celery.schedules import crontab
 
-celery_app = Celery("scheduler")
-celery_app.config_from_object("django.conf:settings", namespace="CELERY")
+celery_app = Celery('scheduler')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
+
+celery_app.conf.beat_schedule = {
+    'apps.users.tasks.ldap_update_users': {
+        'task': 'apps.users.tasks.ldap_update_users',
+        'schedule': crontab(minute='0'),
+        'description': 'This task synchronizes user data against LDAP. If LDAP authentication is not bing used, this task does nothing.'
+    },
+    'apps.logging.tasks.rotate_log_files': {
+        'task': 'apps.logging.tasks.rotate_log_files',
+        'schedule': crontab(hour='0', minute='0'),
+        'description': 'This task deletes old log entries according to application settings.'
+    }
+}
