56e0ae8b6002251226bfaaa007e792b377716c36
Daniel Perrefort <djperrefort@gmail.com>
2024-03-27 18:42:29 -0400

    PEP8 and docstring pass for scheduler app

--- keystone_api/apps/scheduler/__init__.py
+++ keystone_api/apps/scheduler/__init__.py
@@ -1,4 +1,4 @@
 from .celery import celery_app
 
-# The `celery_app` variable must be discoverable by the Celery workers
+# The `celery_app` variable must be defined here to be discovered by Celery workers
 __all__ = ['celery_app']
