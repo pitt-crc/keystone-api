744df4e26cb744166c3a04883fe8866f59170ceb
djperrefort <djperrefort@gmail.com>
2024-04-18 14:42:51 -0400

    Adds tests for logging middleware

--- keystone_api/apps/logging/tests/handlers/test_DBHandler.py
+++ keystone_api/apps/logging/tests/handlers/test_DBHandler.py
@@ -9,9 +9,11 @@ from apps.logging.models import AppLog
 
 
 class EmitToDatabase(TestCase):
-    """Test emitted log data is recorded into the application database."""
+    """Test emitted log data is recorded into the application database"""
 
-    def assert_db_record_matches_log_content(self, handler, log_record, db_record):
+    def assert_db_record_matches_log_content(
+            self, handler: logging.Handler, log_record: logging.LogRecord, db_record: AppLog
+    ) -> None:
         """Assert the content of a database record matches the content of a log record
 
         Args:
@@ -28,7 +30,7 @@ class EmitToDatabase(TestCase):
         self.assertEqual(db_record.func, log_record.funcName)
         self.assertEqual(db_record.sinfo, log_record.stack_info)
 
-    def test_logs_to_database(self):
+    def test_logs_to_database(self) -> None:
         """Test log data is recorded to the application database"""
 
         log_record = logging.LogRecord('test', logging.INFO, 'pathname', 1, 'message', (), None, 'func')
@@ -36,18 +38,19 @@ class EmitToDatabase(TestCase):
         handler.emit(log_record)
 
         db_record = AppLog.objects.first()
+        self.assertEqual(AppLog.objects.count(), 1)
         self.assert_db_record_matches_log_content(handler, log_record, db_record)
 
-    def test_does_not_save_below_level(self):
+    def test_does_not_save_below_level(self) -> None:
         """Test log data is not saved when the log message level is below the logging threshold"""
 
         record = logging.LogRecord('test', logging.INFO, 'pathname', 1, 'message', (), None, 'func')
         handler = DBHandler(level=logging.ERROR)
         handler.emit(record)
 
-        self.assertFalse(AppLog.objects.all())
+        self.assertFalse(AppLog.objects.count())
 
-    def test_saves_equal_to_level(self):
+    def test_saves_equal_to_level(self) -> None:
         """Test log data is saved when the log message level is equal to the logging threshold"""
 
         log_record = logging.LogRecord('test', logging.INFO, 'pathname', 1, 'message', (), None, 'func')
@@ -55,4 +58,5 @@ class EmitToDatabase(TestCase):
         handler.emit(log_record)
 
         db_record = AppLog.objects.first()
+        self.assertEqual(AppLog.objects.count(), 1)
         self.assert_db_record_matches_log_content(handler, log_record, db_record)
