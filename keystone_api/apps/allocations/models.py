1acb6e62cb22f329f7fecd321f18a0b752a618f2
djperrefort <djperrefort@gmail.com>
2024-04-20 13:04:53 -0400

    Adds validation for allocation dates

--- keystone_api/apps/allocations/models.py
+++ keystone_api/apps/allocations/models.py
@@ -10,6 +10,7 @@ from __future__ import annotations
 
 import abc
 
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.template.defaultfilters import truncatechars
 
@@ -71,13 +72,24 @@ class AllocationRequest(RGModelInterface, models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(max_length=1600)
     submitted = models.DateField('Submission Date', auto_now=True)
-    status = models.CharField('Review Status', max_length=2, choices=StatusChoices.choices, default=StatusChoices.PENDING)
+    status = models.CharField('Review Status', max_length=2, choices=StatusChoices.choices,
+                              default=StatusChoices.PENDING)
     active = models.DateField('Active Date', null=True, blank=True)
     expire = models.DateField('Expiration Date', null=True, blank=True)
 
     group: ResearchGroup = models.ForeignKey(ResearchGroup, on_delete=models.CASCADE)
 
     objects = AllocationRequestManager()
+
+    def clean(self) -> None:
+        """Validate the model instance
+
+        Raises:
+            ValidationError: When the model instance data is not valid
+        """
+
+        if self.active >= self.expire:
+            raise ValidationError('The expiration date must come after the activation date.')
 
     def get_research_group(self) -> ResearchGroup:
         """Return the research group tied to the current record"""
