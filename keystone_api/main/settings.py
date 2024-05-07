1e3b7c5f0c30463bd0043bc52cebaabcda492d9b
Nickolas Comeau <nlc60@pitt.edu>
2024-05-07 14:12:07 -0400

    Make JWT lifetime adjustable from env variable (#275)
    * Make JWT lifetime adjustable from env variable
    
    * include refresh token, adjust default duration
    
    * pep8
    
    ---------
    
    Co-authored-by: Daniel Perrefort <djperrefort@gmail.com>

--- keystone_api/main/settings.py
+++ keystone_api/main/settings.py
@@ -229,6 +229,11 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=env.int('SECURE_ACCESS_TOKEN_LIFETIME', 5 * 60)),
+    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=env.int('SECURE_REFRESH_TOKEN_LIFETIME', 24 * 60 * 60))
+}
+
 # Static file handling (CSS, JavaScript, Images)
 
 STATIC_URL = 'static/'
