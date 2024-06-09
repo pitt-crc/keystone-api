3e2a0c7dc31656617782b348a328ceb6cddf8f4b
djperrefort <djperrefort@gmail.com>
2024-06-09 11:19:34 -0400

    Drops duplicate validation logic

--- keystone_api/apps/users/serializers.py
+++ keystone_api/apps/users/serializers.py
@@ -7,6 +7,7 @@ creation.
 """
 
 from django.contrib.auth import password_validation
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from .models import *
@@ -28,59 +29,7 @@ class ResearchGroupSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class RestrictedUserSerializer(serializers.ModelSerializer):
-    """Object serializer for the `User` class with administrative fields marked as read only"""
-
-    class Meta:
-        """Serializer settings"""
-
-        model = User
-        fields = '__all__'
-        read_only_fields = ['is_active', 'is_staff', 'is_ldap_user', 'date_joined', 'last_login']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def validate(self, attrs: dict) -> None:
-        """Validate user attributes match the ORM data model
-
-        Args:
-            attrs: Dictionary of user attributes
-        """
-
-        if 'password' in attrs:
-            password_validation.validate_password(attrs['password'])
-
-        return super().validate(attrs)
-
-    def create(self, validated_data: dict) -> None:
-        """Raises an error when attempting to create a new record
-
-        Raises:
-            RuntimeError: Every time the function is called
-        """
-
-        raise RuntimeError('Attempted to create new user record using a serializer with restricted permissions.')
-
-    def update(self, instance: User, validated_data: dict) -> User:
-        """Update a given database record with the given data
-
-        Args:
-            instance: A `User` record reflecting current database values
-            validated_data: The new values to set on the instance
-
-        Returns:
-            An instance reflecting the new database state
-        """
-
-        # Use the `set_password` method to ensure proper salting/hashing
-        if 'password' in validated_data:
-            password = validated_data.pop('password')
-            password_validation.validate_password(password)
-            instance.set_password(password)
-
-        return super().update(instance, validated_data)
-
-
-class PrivilegeUserSerializer(RestrictedUserSerializer):
+class PrivilegeUserSerializer(serializers.ModelSerializer):
     """Object serializer for the `User` class"""
 
     class Meta:
@@ -91,15 +40,37 @@ class PrivilegeUserSerializer(RestrictedUserSerializer):
         read_only_fields = ['date_joined', 'last_login']
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data: dict) -> User:
-        """Create a new user
+    def validate(self, attrs: dict) -> None:
+        """Validate user attributes match the ORM data model
 
         Args:
-            validated_data: Validated user data
-
-        Returns:
-            A new user instance
+            attrs: Dictionary of user attributes
         """
 
-        # Use `create_user` instead of `create` to ensure passwords are salted/hashed properly
-        return User.objects.create_user(**validated_data)
+        # Hash the password value
+        if 'password' in attrs:
+            password_validation.validate_password(attrs['password'])
+            attrs['password'] = make_password(attrs['password'])
+
+        return super().validate(attrs)
+
+
+class RestrictedUserSerializer(PrivilegeUserSerializer):
+    """Object serializer for the `User` class with administrative fields marked as read only"""
+
+    class Meta:
+        """Serializer settings"""
+
+        model = User
+        fields = '__all__'
+        read_only_fields = ['is_active', 'is_staff', 'is_ldap_user', 'date_joined', 'last_login']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data: dict) -> None:
+        """Raises an error when attempting to create a new record
+
+        Raises:
+            RuntimeError: Every time the function is called
+        """
+
+        raise RuntimeError('Attempted to create new user record using a serializer with restricted permissions.')
