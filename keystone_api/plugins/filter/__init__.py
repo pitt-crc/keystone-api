c5496481e03798bf6512f7f9fbedafd73a126f47
djperrefort <djperrefort@gmail.com>
2024-08-08 12:18:17 -0400

    Doc updates

--- keystone_api/plugins/filter/__init__.py
+++ keystone_api/plugins/filter/__init__.py
@@ -1,11 +1,14 @@
 """Extends the `django-filter` package with custom filter backends.
 
 Filter backends define the default behavior when filtering database queries
-for REST API calls based on URL parameters.
+for REST API calls based on URL parameters. This plugin customizes the default
+filters available for different field types (e.g., char, int, bool, etc.).
 """
 
 from django.db import models
 from django_filters.rest_framework import DjangoFilterBackend
+
+__all__ = ['AdvancedFilterBackend', 'FactoryBuiltFilterSet']
 
 
 class FactoryBuiltFilterSet:
