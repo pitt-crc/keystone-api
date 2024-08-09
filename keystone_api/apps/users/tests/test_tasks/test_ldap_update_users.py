8a761d62538caa4aca4d61f3900506216e4ec83b
djperrefort <djperrefort@gmail.com>
2024-08-09 07:51:50 -0400

    Fill in new tests

--- keystone_api/apps/users/tests/test_tasks/test_ldap_update_users.py
+++ keystone_api/apps/users/tests/test_tasks/test_ldap_update_users.py
@@ -1,7 +1,6 @@
 from unittest.mock import MagicMock, Mock, patch
 
-from django.conf import settings
-from django.test import TestCase
+from django.test import override_settings, TestCase
 
 from apps.users.models import User
 from apps.users.tasks import ldap_update_users
@@ -10,17 +9,22 @@ from apps.users.tasks import ldap_update_users
 class LdapUpdateUsers(TestCase):
     """Test the `ldap_update_users` function."""
 
+    @override_settings(AUTH_LDAP_SERVER_URI=None)
     def test_exit_silently_when_uri_is_none(self) -> None:
         """Test the function exits gracefully when no LDAP server URI is provided."""
 
-        settings.AUTH_LDAP_SERVER_URI = None
         ldap_update_users()
 
+    @override_settings(
+        AUTH_LDAP_SERVER_URI='ldap://ds.example.com:389',
+        AUTH_LDAP_USER_SEARCH=MagicMock(base_dn='dc=example,dc=com'),
+        AUTH_LDAP_USER_ATTR_MAP={'username': 'uid'}
+    )
     @patch('apps.users.tasks.get_ldap_connection')
     def test_users_are_created(self, mock_get_ldap_connection: Mock) -> None:
         """Test that users are updated from LDAP data."""
 
-        # Mock the LDAP search result
+        # Mock LDAP search results
         mock_conn = MagicMock()
         mock_conn.search_s.return_value = [
             ('cn=admin,dc=example,dc=com', {'uid': [b'user1']}),
@@ -28,10 +32,7 @@ class LdapUpdateUsers(TestCase):
         ]
         mock_get_ldap_connection.return_value = mock_conn
 
-        # Configure settings and execute the update
-        settings.AUTH_LDAP_SERVER_URI = 'ldap://ds.example.com:389'
-        settings.AUTH_LDAP_USER_SEARCH = MagicMock(base_dn='dc=example,dc=com')
-        settings.AUTH_LDAP_USER_ATTR_MAP = {'username': 'uid'}
+        # Execute the update
         ldap_update_users()
 
         # Test users were created
@@ -43,12 +44,40 @@ class LdapUpdateUsers(TestCase):
         user2 = User.objects.get(username='user2')
         self.assertTrue(user2.is_ldap_user)
 
-    def test_users_are_pruned(self) -> None:
-        """Test the deletion of missing user accounts"""
+    @override_settings(
+        AUTH_LDAP_SERVER_URI='ldap://ds.example.com:389',
+        AUTH_LDAP_USER_SEARCH=MagicMock(base_dn='dc=example,dc=com'),
+        AUTH_LDAP_USER_ATTR_MAP={'username': 'uid'}
+    )
+    @patch('apps.users.tasks.get_ldap_connection')
+    def test_users_are_pruned(self, mock_get_ldap_connection: Mock) -> None:
+        """Test the deletion of missing user accounts."""
 
-        self.fail()
+        # Mock an LDAP search result with no users
+        mock_conn = MagicMock()
+        mock_conn.search_s.return_value = []
+        mock_get_ldap_connection.return_value = mock_conn
 
-    def test_users_are_deactivated(self) -> None:
-        """Test the deactivation of missing users"""
+        # Test missing users are deleted
+        User.objects.create(username='user_to_prune', is_ldap_user=True)
+        ldap_update_users(prune=True)
+        self.assertFalse(User.objects.filter(username='user_to_prune').exists())
 
-        self.fail()
+    @override_settings(
+        AUTH_LDAP_SERVER_URI='ldap://ds.example.com:389',
+        AUTH_LDAP_USER_SEARCH=MagicMock(base_dn='dc=example,dc=com'),
+        AUTH_LDAP_USER_ATTR_MAP={'username': 'uid'}
+    )
+    @patch('apps.users.tasks.get_ldap_connection')
+    def test_users_are_deactivated(self, mock_get_ldap_connection: Mock) -> None:
+        """Test the deactivation of missing users."""
+
+        # Mock an LDAP search result with no users
+        mock_conn = MagicMock()
+        mock_conn.search_s.return_value = []
+        mock_get_ldap_connection.return_value = mock_conn
+
+        # Test missing users are deactivated
+        User.objects.create(username='user_to_deactivate', is_ldap_user=True, is_active=True)
+        ldap_update_users()
+        self.assertFalse(User.objects.get(username='user_to_deactivate').is_active)
