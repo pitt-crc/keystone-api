25ebd59954fc3e672836faacc6764b7e08c95647
djperrefort <djperrefort@gmail.com>
2024-08-10 11:59:56 -0400

    Add tests for GrantViewSet

--- keystone_api/apps/research_products/tests/test_views/test_GrantViewSet.py
+++ keystone_api/apps/research_products/tests/test_views/test_GrantViewSet.py
@@ -1,46 +1,53 @@
-"""Tests for the `PublicationViewSet` class."""
+"""Tests for the `GrantViewSet` class."""
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from apps.research_products.models import Publication
-from apps.research_products.views import PublicationViewSet
+from apps.research_products.models import Grant
+from apps.research_products.views import GrantViewSet
 from apps.users.models import ResearchGroup
 
 User = get_user_model()
 
 
 class GetQueryset(TestCase):
-    """Test the filtering of records based on user status."""
+    """Test the filtering of grant records based on user status."""
 
     def setUp(self) -> None:
-        """Create user accounts and research publications."""
+        """Create user accounts and research grants."""
 
         self.staff_user = User.objects.create_user(username='staff', password='foobar123!', is_staff=True)
         self.general_user = User.objects.create_user(username='general', password='foobar123!')
 
         self.group1_user = User.objects.create_user(username='user1', password='foobar123!')
         self.group1 = ResearchGroup.objects.create(name='Group1', pi=self.group1_user)
-        self.group1_publication = Publication.objects.create(
-            title="Publication 1",
-            abstract="Abstract 1",
-            date="2020-01-01",
-            journal="Journal 1",
+        self.group1_grant = Grant.objects.create(
+            title="Grant 1",
+            agency="Agency 1",
+            amount=100000.00,
+            grant_number="G-123",
+            fiscal_year=2020,
+            start_date="2020-01-01",
+            end_date="2021-01-01",
             group=self.group1
         )
 
         self.group2_user = User.objects.create_user(username='user2', password='foobar123!')
         self.group2 = ResearchGroup.objects.create(name='Group2', pi=self.group2_user)
-        self.group2_publication = Publication.objects.create(
-            title="Publication 2",
-            abstract="Abstract 2",
-            date="2020-01-02",
-            journal="Journal 2",
+        self.group2_grant = Grant.objects.create(
+            title="Grant 2",
+            agency="Agency 2",
+            amount=200000.00,
+            grant_number="G-456",
+            fiscal_year=2021,
+            start_date="2021-01-01",
+            end_date="2022-01-01",
             group=self.group2
         )
 
-    def create_viewset(self, user: User) -> PublicationViewSet:
-        """Create a viewset for testing purposes
+    def create_viewset(self, user: User) -> GrantViewSet:
+        """
+        Create a viewset for testing purposes.
 
         Args:
             user: The user submitting a request to the viewset
@@ -49,20 +56,20 @@ class GetQueryset(TestCase):
             A viewset instance tied to a request from the given user
         """
 
-        viewset = PublicationViewSet()
+        viewset = GrantViewSet()
         viewset.request = self.client.request().wsgi_request
         viewset.request.user = user
         return viewset
 
     def test_queryset_for_staff_user(self) -> None:
-        """Test staff users can view all publications."""
+        """Test staff users can view all grants."""
 
         viewset = self.create_viewset(self.staff_user)
         queryset = viewset.get_queryset()
         self.assertEqual(len(queryset), 2)
 
     def test_queryset_for_group_member(self) -> None:
-        """Test user's can only access their group's publications."""
+        """Test group members can only access their group's grants."""
 
         viewset = self.create_viewset(self.group1_user)
         queryset = viewset.get_queryset()
@@ -70,7 +77,7 @@ class GetQueryset(TestCase):
         self.assertEqual(queryset[0].group, self.group1)
 
     def test_queryset_for_non_group_member(self) -> None:
-        """Test user's without groups cannot access any records."""
+        """Test users without groups cannot access any grant records."""
 
         viewset = self.create_viewset(self.general_user)
         queryset = viewset.get_queryset()
