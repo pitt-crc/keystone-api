7fd0d97aa4d2abc2b043199af8d7b9e8952376ca
djperrefort <djperrefort@gmail.com>
2024-08-22 11:25:02 -0400

    Adds tests for review record creation

--- keystone_api/apps/allocations/tests/test_views/test_AllocationRequestReviewViewSet.py
+++ keystone_api/apps/allocations/tests/test_views/test_AllocationRequestReviewViewSet.py
@@ -1,6 +1,7 @@
 """Unit tests for the `AllocationRequestReviewViewSet` class."""
 
 from django.test import RequestFactory, TestCase
+from rest_framework import status
 
 from apps.allocations.models import AllocationRequest, AllocationRequestReview
 from apps.allocations.views import AllocationRequestReviewViewSet
@@ -53,12 +54,61 @@ class GetQueryset(TestCase):
 class Create(TestCase):
     """Test the creation of new records."""
 
-    def test_create_with_automatic_reviewer(self, ) -> None:
+    # TODO: Move into fixture
+    def setUp(self) -> None:
+        self.staff_user = User.objects.create_user('staff', 'foobar123!', is_staff=True)
+
+        self.user1 = User.objects.create_user('user1', 'foobar123!', is_staff=False)
+        self.group1 = ResearchGroup.objects.create(name='group1', pi=self.user1)
+        self.request1 = AllocationRequest.objects.create(title='request1', group=self.group1)
+
+    def test_create_with_automatic_reviewer(self) -> None:
         """Test the reviewer field is automatically set to the current user."""
 
-        self.fail()
+        request = RequestFactory().post('/allocation-reviews/')
+        request.user = self.staff_user
+        request.data = {
+            'request': self.request1.id,
+            'status': 'AP'
+        }
+
+        viewset = AllocationRequestReviewViewSet()
+        viewset.request = request
+        viewset.format_kwarg = None
+
+        # Test the returned response data
+        response = viewset.create(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['reviewer'], self.staff_user.id)
+
+        # Test the created DB record
+        review = AllocationRequestReview.objects.get(pk=response.data['id'])
+        self.assertEqual(review.reviewer, self.staff_user)
+        self.assertEqual(review.request, self.request1)
+        self.assertEqual(review.status, 'AP')
 
     def test_create_with_provided_reviewer(self, ) -> None:
         """Test the reviewer field in the request data is respected if provided."""
 
-        self.fail()
+        request = RequestFactory().post('/allocation-reviews/')
+        request.user = self.staff_user
+        request.data = {
+            'request': self.request1.id,
+            'reviewer': self.staff_user.id,
+            'status': 'AP'
+        }
+
+        viewset = AllocationRequestReviewViewSet()
+        viewset.request = request
+        viewset.format_kwarg = None
+
+        # Test the returned response data
+        response = viewset.create(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['reviewer'], self.staff_user.id)
+
+        # Test the created DB record
+        review = AllocationRequestReview.objects.get(pk=response.data['id'])
+        self.assertEqual(review.reviewer, self.staff_user)
+        self.assertEqual(review.request, self.request1)
+        self.assertEqual(review.status, 'AP')
