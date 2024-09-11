4cee633ecd53b81665c5101644e2b27c024303a0
djperrefort <djperrefort@gmail.com>
2024-09-11 12:20:10 -0400

    Update openapi spec

--- keystone_api/apps/authentication/views.py
+++ keystone_api/apps/authentication/views.py
@@ -4,17 +4,19 @@ View objects handle the processing of incoming HTTP requests and return the
 appropriately rendered HTML template or other HTTP response.
 """
 
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from apps.users.serializers import RestrictedUserSerializer
 
 __all__ = ['WhoAmIView']
 
 
-class WhoAmIView(APIView):
+class WhoAmIView(GenericAPIView):
     """Return user metadata for the currently authenticated user."""
 
+    serializer_class = RestrictedUserSerializer
     permission_classes = []
 
     def get(self, request, *args, **kwargs) -> Response:
@@ -25,7 +27,7 @@ class WhoAmIView(APIView):
         """
 
         if not request.user.is_authenticated:
-            return Response(status=404)
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-        serializer = RestrictedUserSerializer(request.user)
+        serializer = self.serializer_class(request.user)
         return Response(serializer.data)
