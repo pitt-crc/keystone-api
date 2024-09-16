15e342c4f381f65cf4eb53156a0a720d96735ba4
djperrefort <djperrefort@gmail.com>
2024-09-16 14:49:14 -0400

    Revert changes to settings.py

--- keystone_api/main/settings.py
+++ keystone_api/main/settings.py
@@ -30,23 +30,17 @@ FIXTURE_DIRS = [BASE_DIR / 'tests' / 'fixtures']
 SECRET_KEY = os.environ.get('SECURE_SECRET_KEY', get_random_secret_key())
 ALLOWED_HOSTS = env.list("SECURE_ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
-_SECURE_SSL_TOKENS = env.bool("SECURE_SSL_TOKENS", False)
-SESSION_COOKIE_SECURE = _SECURE_SSL_TOKENS
-SESSION_COOKIE_HTTPONLY = True
-SESSION_COOKIE_SAMESITE = "Lax"
-SESSION_COOKIE_AGE = env.int("SECURE_SESSION_AGE", 1209600)  # 2 weeks
-
+_SECURE_SESSION_TOKENS = env.bool("SECURE_SESSION_TOKENS", False)
+SESSION_COOKIE_SECURE = _SECURE_SESSION_TOKENS
+CSRF_COOKIE_SECURE = _SECURE_SESSION_TOKENS
 CSRF_TRUSTED_ORIGINS = env.list("SECURE_CSRF_ORIGINS", default=[])
-CSRF_COOKIE_SECURE = _SECURE_SSL_TOKENS
-CSRF_COOKIE_HTTPONLY = False
-CSRF_COOKIE_SAMESITE = "Lax"
 
 SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", False)
 SECURE_HSTS_PRELOAD = env.bool("SECURE_HSTS_PRELOAD", False)
 SECURE_HSTS_SECONDS = env.int("SECURE_HSTS_SECONDS", 0)
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("SECURE_HSTS_SUBDOMAINS", False)
 
-CORS_ALLOWED_ORIGINS = env.list("SECURE_ALLOWED_ORIGINS", default=[])
+CORS_ORIGIN_ALLOW_ALL = True
 
 # App Configuration
 
@@ -56,7 +50,6 @@ SITE_ID = 1
 
 INSTALLED_APPS = [
     'jazzmin',
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -72,8 +65,7 @@ INSTALLED_APPS = [
     'health_check.contrib.celery_ping',
     'health_check.contrib.redis',
     'rest_framework',
-    'rest_framework.authtoken',
-    'dj_rest_auth',
+    'rest_framework_simplejwt.token_blacklist',
     'django_celery_beat',
     'django_celery_results',
     'django_filters',
@@ -94,12 +86,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'apps.logging.middleware.LogRequestMiddleware',
 ]
@@ -147,6 +139,9 @@ JAZZMIN_SETTINGS = {
 # REST API settings
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
     ],
@@ -261,6 +256,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=env.int('SECURE_ACCESS_TOKEN_LIFETIME', 5 * 60)),
+    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=env.int('SECURE_REFRESH_TOKEN_LIFETIME', 24 * 60 * 60)),
+    'UPDATE_LAST_LOGIN': True
+}
 
 # Static file handling (CSS, JavaScript, Images)
 
